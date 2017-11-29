b61ce8134749b16922120ecb6c8857e6a439dc4f
marsz <marsz330@gmail.com>
2017-11-29 12:47:51 +0800

    build with uncompress js

--- app/compilers/js_compiler.rb
+++ app/compilers/js_compiler.rb
@@ -1,17 +1,20 @@
 class JsCompiler
   class << self
 
-    def compile(name, controller: nil, assigns: {}, view_path: nil)
+    def compile(name, compress: true, controller: nil, assigns: {}, view_path: nil)
       assigns = default_assigns(name).merge(assigns)
       controller ||= default_controller(name)
       view_path ||= default_view_path(name)
       view = ActionView::Base.new(controller.view_paths, assigns)
-      Uglifier.compile(view.render(file: view_path))
+      code = view.render(file: view_path)
+      compress ? Uglifier.compile(code) : code
     end
 
-    def to_file(name, dist = nil, compile_params: {})
-      dist ||= Rails.root.join('public', 'dist', "#{name.to_s.downcase}.js")
+    def to_file(name, compile_params: {})
+      dist = Rails.root.join('public', 'dist', "#{name.to_s.downcase}.min.js")
       IO.write(dist, compile(name, compile_params.symbolize_keys))
+      dist = Rails.root.join('public', 'dist', "#{name.to_s.downcase}.js")
+      IO.write(dist, compile(name, compile_params.symbolize_keys.merge(compress: false)))
     end
 
     private
